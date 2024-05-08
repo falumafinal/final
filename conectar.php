6b7597a5a538fa4ebe29b37470268ec87295dd15
falumafinal <165970507+falumafinal@users.noreply.github.com>
2024-05-08 20:37:08 -0300

    Update conectar.php

--- conectar.php
+++ conectar.php
@@ -1,1 +1,12 @@
+<?php
+// Conexión a la base de datos
+$conexion = mysqli_connect("localhost", "root", "", "registro");
+
+// Verificar conexión
+if (!$conexion) {
+    die("Error de conexión: " . mysqli_connect_error());
+}
+// Cerrar la conexión a la base de datos
+mysqli_close($conexion);
+?>
 
